57f79c57c609105551bcb854b18773f999cb767b
Mark Caron <mcaron@redhat.com>
2018-06-27 13:34:39 -0400

    Adding rh-avatar

---
+++ elements/rh-avatar/rh-avatar.story.js
@@ -0,0 +1,11 @@
+import { storiesOf } from "@storybook/polymer";
+import "./rh-avatar";
+
+storiesOf("Avatar", module).add(
+  "rh-avatar",
+  () => `
+  <rh-avatar>
+    Avatar
+  </rh-avatar>
+  `
+);
